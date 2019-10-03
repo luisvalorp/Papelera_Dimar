c4df1ff7409c25fbc53cbf86e763cee7646541e7
luisvalorp <52467543+luisvalorp@users.noreply.github.com>
2019-10-03 15:52:15 -0400

    Update Base.java

--- src/test/java/Commons/Base.java
+++ src/test/java/Commons/Base.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.Platform;
+import java.net.MalformedURLException;
 
 import java.io.File;
 import java.net.URL;
