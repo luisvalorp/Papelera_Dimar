11b2f6e29023152c4fc39f0af225580fded59e8c
luisvalorp <52467543+luisvalorp@users.noreply.github.com>
2019-10-14 14:55:42 -0400

    Update Base.java

--- src/test/java/Commons/Base.java
+++ src/test/java/Commons/Base.java
@@ -2,16 +2,16 @@ package Commons;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.Platform;
-import java.net.MalformedURLException;
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Base {
